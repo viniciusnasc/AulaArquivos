970ab46c1edc9304ae478b683ec624f8d146521b
Vinicius Nascimento <83601645+viniciusnasc@users.noreply.github.com>
2021-08-14 01:42:42 -0300

    Resolução do exercicio

--- ExercicioFixacao/Program.cs
+++ ExercicioFixacao/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 
 namespace ExercicioFixacao
@@ -7,20 +8,49 @@ namespace ExercicioFixacao
     {
         static void Main(string[] args)
         {
-            // Local da pasta principal
-            string principalPastPath = @"C:\Users\Vini\AulaArquivos";
-
-            // Criar o arquivo principal
-            string principalFilePath = @"C:\Users\Vini\AulaArquivos\principal.csv";
-            FileInfo principalFile = new(principalFilePath);
-
-            using (StreamWriter sw = File.AppendText(principalFilePath))
+            try
             {
-                sw.WriteLine("TV LED, 1290.99, 1");
-                sw.WriteLine("Video Game Chair, 350.50, 3");
-                sw.WriteLine("Iphone X, 900.00, 2");
-                sw.WriteLine("Samsung Galaxy 9, 850.00, 2");
+                // Local da pasta principal
+                string principalFolderPath = @"C:\Users\Vini\AulaArquivos";
+
+                // Criar o arquivo principal
+                string principalFilePath = @"C:\Users\Vini\AulaArquivos\principal.csv";
+                FileInfo principalFile = new(principalFilePath);
+
+                using (StreamWriter sw = File.AppendText(principalFilePath))
+                {
+                    sw.WriteLine("TV LED, 1290.99, 1");
+                    sw.WriteLine("Video Game Chair, 350.50, 3");
+                    sw.WriteLine("Iphone X, 900.00, 2");
+                    sw.WriteLine("Samsung Galaxy 9, 850.00, 2");
+                }
+
+                // Criar a subpasta
+                string subFolderPath = principalFolderPath + @"\out";
+                Directory.CreateDirectory(subFolderPath);
+
+                // Criar o segundo arquivo
+                string secFilePath = subFolderPath + @"\summary.csv";
+                FileInfo secondFile = new(secFilePath);
+
+                string[] lines = File.ReadAllLines(principalFilePath);
+
+                using (StreamWriter sw = File.AppendText(secFilePath))
+                {
+                    foreach (string x in lines)
+                    {
+                        string[] linesSplit = x.Split(",");
+                        double valor = double.Parse(linesSplit[1].Trim(), CultureInfo.InvariantCulture) * double.Parse(linesSplit[2].Trim(), CultureInfo.InvariantCulture);
+                        sw.WriteLine(linesSplit[0] + ", " + valor.ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
             }
         }
     }
+
 }
